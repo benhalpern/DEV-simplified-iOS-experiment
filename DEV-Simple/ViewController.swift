65bbad96f584277faac52c1d6af09a8cb1690a86
Ben Halpern <ben@dev.to>
2018-11-01 17:24:00 -0400

    Initial Commit

---
+++ DEV-Simple/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DEV-Simple
+//
+//  Created by Ben Halpern on 11/1/18.
+//  Copyright © 2018 DEV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
